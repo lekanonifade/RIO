648ef013669443ae34992853cd8c4c553e180683
Olamilekan Onifade <ayomikunolamilekan@gmail.com>
2023-12-14 18:10:42 +0100

    update

--- src/structure/header/header.jsx
+++ src/structure/header/header.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faBell, faEnvelope, faMoon } from "@fortawesome/free-solid-svg-icons";
+import { Avatar, AvatarBadge, Text } from "@chakra-ui/react";
+import { BellIcon, EmailIcon, MoonIcon } from "@chakra-ui/icons";
 
 export default function Header() {
     return (
         <div className="flex justify-between fixed w-full p-2 bg-white">
-            <p><FontAwesomeIcon icon={faBars}/></p>
-            <div className="flex justify-items-end text-2xl gap-x-3 mt-2">
-                <p><FontAwesomeIcon icon={faMoon} /></p>
-                <p><FontAwesomeIcon icon={faBell} /></p>
-                <p><FontAwesomeIcon icon={faEnvelope} /></p>
-                <img src="../images/header/lekan.png" className="w-10 h-10 -mt-1 rounded-full" />
+            <p><FontAwesomeIcon icon={faBars} /></p>
+            <div className="flex justify-items-end text-2xl gap-x-3 mt-3">
+                <MoonIcon />
+                <BellIcon />
+                <EmailIcon />
+                <Avatar name="daffod" src="../images/header/lekan.png" className="-mt-3">
+                    <AvatarBadge width={"1.3em"} height={"1.3em"} bg={"teal.500"}>
+                        <Text fontSize={'xs'}>3</Text>
+                    </AvatarBadge>
+                </Avatar>
             </div>
         </div>
     )
