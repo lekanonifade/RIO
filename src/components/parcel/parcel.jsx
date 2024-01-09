0313974402f1aa78b5b9e132d320e62e6c054f3c
Olamilekan Onifade <ayomikunolamilekan@gmail.com>
2024-01-09 14:25:50 +0100

    update

--- src/components/parcel/parcel.jsx
+++ src/components/parcel/parcel.jsx
@@ -10,31 +10,31 @@ export default function Parcel() {
                         <div className="grid grid-cols-2 gap-10">
                             <form className="">
                                 <p className="p-1">Name</p>
-                                <input type="text" placeholder="Enter Your ID" className="border rounded-xl w-full h-10 p-3" />
+                                <input type="text" placeholder="Enter Your ID" className="border border-gray-300 outline-none rounded-xl w-full h-10 p-3" />
                             </form>
                             <div className="">
                                 <p className="p-1">Company Name</p>
                                 <div className="flex gap-10">
                                     <button className="border border-[#ED1C24] w-full rounded-xl h-10 bg-[#FBD0D1] text-[#ED1C24]">Solid</button>
-                                    <button className="border w-full rounded-xl h-10">Liquid</button>
-                                    <button className="border w-full rounded-xl h-10">Fragile</button>
+                                    <button className="border border-gray-300 w-full rounded-xl h-10">Liquid</button>
+                                    <button className="border border-gray-300 w-full rounded-xl h-10">Fragile</button>
                                 </div>
                             </div>
                             <form className="">
                                 <p className="p-1">Phone Number</p>
-                                <input type="text" placeholder="Enter Your Phone Number" className="border rounded-xl w-full h-10 p-3" />
+                                <input type="text" placeholder="Enter Your Phone Number" className="border border-gray-300 outline-none rounded-xl w-full h-10 p-3" />
                             </form>
                             <form className="">
                                 <p className="p-1">Weight</p>
-                                <input type="text" placeholder="G/Kg" className="border rounded-xl w-full h-10 p-3" />
+                                <input type="text" placeholder="G/Kg" className="border border-gray-300 outline-none rounded-xl w-full h-10 p-3" />
                             </form>
                             <form className="">
                                 <p className="p-1">Address</p>
-                                <textarea type="text" placeholder="Recipient Address" className="border rounded-xl w-full h-40 p-3" />
+                                <textarea type="text" placeholder="Recipient Address" className="border border-gray-300 outline-none rounded-xl w-full h-40 p-3" />
                             </form>
                             <form className="">
                                 <p className="p-1">Note</p>
-                                <textarea type="text" placeholder="Type your note" className="border rounded-xl w-full h-40 p-3" />
+                                <textarea type="text" placeholder="Type your note" className="border border-gray-300 outline-none rounded-xl w-full h-40 p-3" />
                             </form>
                         </div>
                         <button className="w-[50%] ml-[25%] bg-[#F03D43] rounded-xl h-10 text-white">Login</button>
@@ -52,7 +52,7 @@ export default function Parcel() {
                         <p>Delivery Change</p>
                         <p>$200.00</p>
                     </div>
-                    <div className="flex justify-between mt-5 border p-1">
+                    <div className="flex justify-between mt-5 border border-gray-300 p-1">
                         <p>Total Amount</p>
                         <p>$200.00</p>
                     </div>
