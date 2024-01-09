0a34fc5296e480bd681e8d209c813845955a980f
Olamilekan Onifade <ayomikunolamilekan@gmail.com>
2024-01-09 14:26:52 +0100

    update

--- src/components/account/accountProps.jsx
+++ src/components/account/accountProps.jsx
@@ -4,7 +4,7 @@ export default function Accountprop(props) {
     return (
         <div className="">
             <p className="p-1">{props.accountTitle}</p>
-            <input type="text" placeholder={props.accountPlaceholder} className="border rounded-xl w-full h-10 p-3"/>
+            <input type="text" placeholder={props.accountPlaceholder} className="border border-gray-300 outline-none rounded-xl w-full h-10 p-3"/>
         </div>
     )
 }
