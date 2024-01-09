2bc8ded5ee8d1de94c698442b79b27fb9764edbd
Olamilekan Onifade <ayomikunolamilekan@gmail.com>
2024-01-09 14:27:07 +0100

    update

--- src/components/settings/settingsProps.jsx
+++ src/components/settings/settingsProps.jsx
@@ -4,7 +4,7 @@ export default function Settingsprop(props) {
     return (
         <div className="">
             <p className="p-1">{props.settingsTitle}</p>
-            <input type="text" placeholder={props.settingsPlaceholder} className="border rounded-xl w-full h-10 p-3"/>
+            <input type="text" placeholder={props.settingsPlaceholder} className="border border-gray-300 outline-none rounded-xl w-full h-10 p-3"/>
         </div>
     )
 }
